815f364c598a4e1ccaf64677510c1c3af92c646d
sistemas.madrid <sistemas.madrid@host.docker.internal>
2024-12-12 19:35:18 +0100

    Inicio

---
+++ dtos/src/main/java/com/arquitecturajava/webspring/mappers/PersonaMapper.java
@@ -0,0 +1,17 @@
+package com.arquitecturajava.webspring.mappers;
+
+import com.arquitecturajava.webspring.dtos.PersonaDto;
+import com.arquitecturajava.webspring.models.Persona;
+
+public class PersonaMapper {
+
+	public static PersonaDto toDto(Persona persona) {
+		
+		return new PersonaDto(persona.getNombre());
+	}
+	
+	public static Persona toBo(PersonaDto personaDto) {
+		
+		return new Persona(personaDto.getNombre());
+	}
+}
